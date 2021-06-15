32356cf4693eb3826227c2628bbd7fa1b5e85f94
Yessenia <vela.kathy@gmail.com>
2021-06-15 18:45:14 -0500

    aprende con angular

---
+++ burger-queen/src/app/bar-side-waiter-component/bar-side-waiter-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-side-waiter-component',
+  templateUrl: './bar-side-waiter-component.component.html',
+  styleUrls: ['./bar-side-waiter-component.component.css']
+})
+export class BarSideWaiterComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
