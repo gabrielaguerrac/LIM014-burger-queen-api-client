12a59a4432dffa8c64a5c838f16d83dec226483f
Yessenia <vela.kathy@gmail.com>
2021-06-17 12:09:46 -0500

    componente de navegación

---
+++ burger-queen/src/app/bar-side/bar-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bar-side',
+  templateUrl: './bar-side.component.html',
+  styleUrls: ['./bar-side.component.css']
+})
+export class BarSideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
