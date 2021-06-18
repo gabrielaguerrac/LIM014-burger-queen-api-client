4018d7ba56dea018bbbbfbbe2b0cc16e6d8318f9
Yessenia <vela.kathy@gmail.com>
2021-06-18 11:40:54 -0500

    archivos y escritura del ruteo

---
+++ burger-queen/src/app/productivity-kitchen/productivity-kitchen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productivity-kitchen',
+  templateUrl: './productivity-kitchen.component.html',
+  styleUrls: ['./productivity-kitchen.component.css']
+})
+export class ProductivityKitchenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
