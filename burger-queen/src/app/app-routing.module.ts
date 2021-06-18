019eb4bdf41cab60004262bc71561d93f7ec8af7
Yessenia <vela.kathy@gmail.com>
2021-06-18 08:56:42 -0500

    agrega comentarios para entender de que va ese archivo

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Importar componentes
+/* import {  } */
+
+// Array de rutas
 const routes: Routes = [];
 
 @NgModule({
+  // cargamos todas las configuraciones de la ruta
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
