7306c0f8269f5bef072cec53cc404e9f1d9f1f33
Yessenia <vela.kathy@gmail.com>
2021-06-18 11:40:22 -0500

    se empieza el ruteo de las vistas

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Importar componentes
 import { LoginComponent } from './login/login.component';
-import { BarSideComponent } from './bar-side/bar-side.component';
-
+// import { BarSideComponent } from './bar-side/bar-side.component';
+import { UserComponent } from './user/user.component';
+// import { ProductsComponent } from './products/products.component';
+// import { WaiterTablesComponent } from './waiter-tables/waiter-tables.component';
+// import { OrdersComponent } from './orders/orders.component';
+// import { OrdersKitchenComponent } from './orders-kitchen/orders-kitchen.component';
 // Array de rutas
 const routes: Routes = [
   {path: '', component: LoginComponent}, //por default
+  {path: 'user', component: UserComponent},
+  // {path: 'products', component: ProductsComponent},
+  // {path: 'waitertables', component: WaiterTablesComponent},
+  // {path: 'orders', component: OrdersComponent},
+  // {path: 'orderskitchen', component: OrdersComponent},
   {path: '**', component: LoginComponent} //mientras login hasta tener un componente de error
-
 ];
 
 @NgModule({
