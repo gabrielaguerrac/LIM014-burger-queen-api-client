b475704a034685b61d1bd8deb4f6d664277e5363
Yessenia <vela.kathy@gmail.com>
2021-06-18 11:36:35 -0500

    con credenciales correctas, se hace click en el boton login y te muestra la sgte vista

--- burger-queen/src/app/login/login.component.ts
+++ burger-queen/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component , OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { Form } from '@angular/forms';
 import { Token } from '../models/auth';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'login',
@@ -14,10 +15,16 @@ export class LoginComponent implements OnInit {
   email= "";
   password="";
   // método que se ejecuta cuando carga un objeto
-  constructor(private authService: AuthService){ 
-  }
+  constructor(
+      private authService: AuthService,
+      private route: ActivatedRoute,
+      private router: Router 
+    ){ }
   // método que permite iniciar el componente luego del constructor
   ngOnInit(){}
+  redirigir(){
+    this.router.navigate(['/user']);
+}
 
   public sendCredentials(form: any){
       this.authService.postCredential({email: this.email, password: this.password})  
@@ -25,7 +32,11 @@ export class LoginComponent implements OnInit {
           console.log(response);
           // get email
           //add el cambio de pantalla
+        // si el usuario es admin: true, entonces que se cumplea el redirigir a user
+        this.redirigir();
       }) 
       form.reset();
   }
+
+  
 }
