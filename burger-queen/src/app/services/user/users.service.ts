a8384a93d5fef046baa299aeb0a49ed321fa8cd0
Yessenia <vela.kathy@gmail.com>
2021-06-19 08:27:16 -0500

    crea el componente para la página de error 404

--- burger-queen/src/app/services/user/users.service.ts
+++ burger-queen/src/app/services/user/users.service.ts
@@ -1,27 +1,30 @@
 // NOTAS:
 // un servicio es una clase
-// su objetivo es separar la lógica principal de la app
-// tienen métodos que harán funciones y peticiones ayax al backend
-// será modelo de consulta
-// tiene un objeto inyectable
+// un servicio es un proveedor de datos, que mantiene lógica de acceso a ellos 
+// y operativa relacionada con el negocio y las cosas que se hacen con 
+// los datos dentro de una aplicación. Los servicios serán consumidos por 
+// los componentes, que delegarán en ellos la responsabilidad de acceder a la 
+// información y la realización de operaciones con los datos.
 import { Injectable } from '@angular/core';
 // es un decorador que sirve para inyectar nuestro objeto en otra clase
 // y no estarlo creando siempre
 import { HttpClient } from "@angular/common/http";
 import { HttpHeaders } from "@angular/common/http";
+import { environment } from '../../../environments/environment';
 import { LoginComponent } from "../../login/login.component";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
     providedIn: "root"
 })
 
 export class UsersService {
+    private domain: string;
+    private endpoint: string;
     constructor(private httpclient: HttpClient) {
-        console.log('constructor de loginService');
-        
+        console.log('constructor AQUÍ!!!');
+        this.domain = environment.domain;
+        this.endpoint = '/user';
     }
-
-    // public post(url: string, body){
-    //     return this.httpclient.post(url, body);
-    // }
 }
