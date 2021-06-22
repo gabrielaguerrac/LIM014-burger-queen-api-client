a6a552cd403ced0bec220519d200951ade388460
Yessenia <vela.kathy@gmail.com>
2021-06-21 19:34:02 -0500

    incluye un path de user/:id

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Importar componentes
 import { LoginComponent } from './login/login.component';
+import { Page404Component } from './page404/page404.component';
 // import { BarSideComponent } from './bar-side/bar-side.component';
 import { UserComponent } from './user/user.component';
 // import { ProductsComponent } from './products/products.component';
@@ -12,12 +13,14 @@ import { UserComponent } from './user/user.component';
 // Array de rutas
 const routes: Routes = [
   {path: '', component: LoginComponent}, //por default
+  // { path: '/offers', component: OffersComponent } // TO DO: only users auth
   {path: 'user', component: UserComponent},
+  {path: 'user/:id', component: UserComponent},
   // {path: 'products', component: ProductsComponent},
   // {path: 'waitertables', component: WaiterTablesComponent},
   // {path: 'orders', component: OrdersComponent},
   // {path: 'orderskitchen', component: OrdersComponent},
-  {path: '**', component: LoginComponent} //mientras login hasta tener un componente de error
+  {path: '**', component: Page404Component} //mientras login hasta tener un componente de error
 ];
 
 @NgModule({
