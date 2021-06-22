847491734ac4e79abfdb45ac03f85934d1198f01
Yessenia <vela.kathy@gmail.com>
2021-06-21 19:38:00 -0500

    acomoda los tipos de datos

--- burger-queen/src/app/models/user.model.ts
+++ burger-queen/src/app/models/user.model.ts
@@ -1,21 +1,23 @@
 // Este el modelo que va representar a un solo usuario a un solo objeto
-
+export interface UserIdModel {
+  // user?: UserDetailModel;
+  user?: UserModel;
+}
 export interface UserModel {
     id: string,
     email: string,
-    password: string,
-    admin: boolean,
+    /* password: string, */
+    roles: {
+      admin: boolean
+    }
 }
-
 //
-export interface IUserModel {
-    user?: UserDetailModel;
-  }
-  export interface UserDetailModel {
-    _id?: string;
-    email?: string;
-    roles: RolModel;
-  }
-  export interface RolModel {
-    admin: boolean;
-  }
+
+// export interface UserDetailModel {
+//   _id?: string;
+//   email?: string;
+//   roles: RolModel;
+// }
+export interface RolModel {
+  admin: boolean;
+}
