5ecccf2f87254b194e4f33cd8684f3e47b8ce97a
Yessenia <vela.kathy@gmail.com>
2021-06-21 19:39:48 -0500

    incluye líneas para obtener el token y lograr hacer un login

--- burger-queen/src/app/services/auth/auth.service.ts
+++ burger-queen/src/app/services/auth/auth.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Auth, Token } from '../../models/auth';
+import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+import { UserModel } from 'src/app/models/user.model';
+/* import { isNullOrUndefined } from 'util'; */
+/* import { isNull } from '@angular/compiler/src/output/output_ast'; */
 
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
-  url = "http://localhost:4201/auth"
-  
+  /* private domain: string; */
+  private endpoint: string;
+  url = "http://localhost:4201"
   constructor(private http: HttpClient) {
+    /* this.domain = environment.domain; */
+    this.endpoint = '/auth';
    }
-  
-  postCredential(data: Auth): Observable<Token> {
-    return this.http.post<Token>(this.url, data);
+  loginUser (data: Auth): Observable<Token> {
+    return this.http.post<Token>(`${this.url}${this.endpoint}`, data);
   }
+  // loginUser(data: Auth): Observable<Token> {
+  //   return this.http.post<Token>(`${this.url}${this.endpoint}`, data);
+  //   // return this.http.post<Token>(`${this.domain}${this.endpoint}`, data);
+  // }
+  
 }
