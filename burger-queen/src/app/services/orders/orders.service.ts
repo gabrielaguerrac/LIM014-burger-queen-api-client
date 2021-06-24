ec3238df145efc670e3b56bd46772a96565baa2b
gabrielaguerrac <gguerracast@gmail.com>
2021-06-23 19:52:07 -0500

    metodo deliverOrder declarado

---
+++ burger-queen/src/app/services/orders/orders.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Token } from 'src/app/models/auth';
+import { OrdersComponent } from 'src/app/orders/orders.component';
+import { Auth } from 'src/app/models/auth';
+import { Observable } from 'rxjs';
+import { Orders } from 'src/app/models/orders';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  private endpoint: string;
+  url = "http://localhost:4201"
+  constructor(private http: HttpClient) {
+    /* this.domain = environment.domain; */
+    this.endpoint = '/orders';
+   }
+
+  getAllOrders (): Observable<Orders> {
+    return this.http.get<Orders>(`${this.url}${this.endpoint}`);
+    }
+}
