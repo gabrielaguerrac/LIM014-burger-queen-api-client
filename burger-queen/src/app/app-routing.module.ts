dc8841000b32d5ca9833825ef7dfa965c9945c1e
gabrielaguerrac <gguerracast@gmail.com>
2021-06-28 19:50:51 -0500

    routing cambio irrelevante

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { UserComponent } from './user/user.component';
 import { RoleSelectorComponent } from './role-selector/role-selector.component';
 import { ProductsComponent } from './products/products.component';
 import { WaiterTablesComponent } from './waiter-tables/waiter-tables.component';
-//import { OrdersComponent } from './orders/orders.component';
+import { OrdersComponent } from './orders/orders.component';
 import { OrdersKitchenComponent } from './orders-kitchen/orders-kitchen.component';
 
 // Array de rutas
@@ -21,7 +21,7 @@ const routes: Routes = [
   {path: 'roleselector', component: RoleSelectorComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'waitertables', component: WaiterTablesComponent},
-  // {path: 'orders', component: OrdersComponent},
+  {path: 'orders', component: OrdersComponent},
   {path: 'orderskitchen', component: OrdersKitchenComponent},
   {path: '**', component: Page404Component} //mientras login hasta tener un componente de error
 
