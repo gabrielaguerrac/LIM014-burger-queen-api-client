957ac78a932e9b47c68753d661454ef5a86eecec
Yessenia <vela.kathy@gmail.com>
2021-07-05 11:13:12 -0500

    se muestra la barra de navegación para el mesero y administrador

--- burger-queen/src/app/bar-side/bar-side.component.ts
+++ burger-queen/src/app/bar-side/bar-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import jwtDecode from 'jwt-decode';
 
 @Component({
   selector: 'bar-side',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-side.component.css']
 })
 export class BarSideComponent implements OnInit {
+  
+  isAdmin: boolean=true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getRole();
+  }
+
+  getRole(){
+    const token: any = localStorage.getItem('accessToken');
+    const role: any = jwtDecode(token);
+
+    if(role.roles.admin){
+      // console.log(role.roles.admin);
+      this.isAdmin;
+      // console.log(this.isAdmin);
+    } else {
+      this.isAdmin =! this.isAdmin;
+    }
   }
 
 }
--- burger-queen/src/app/products/products.component.ts
+++ burger-queen/src/app/products/products.component.ts
@@ -150,7 +150,6 @@ export class ProductsComponent implements OnInit {
     }
     )
     console.log(order);
-    
   }
   
 
